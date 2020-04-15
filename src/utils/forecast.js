8728ca520141151281258df347dcc0221e8521fa
Roland <roland.theisen@outlook.com>
2020-04-15 11:35:06 +0200

    Added humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,26 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (lat, long, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=099af8905628aa3beefc660743b9cd5d&query=${lat},${long}&units=m`
+    const url = `http://api.weatherstack.com/current?access_key=099af8905628aa3beefc660743b9cd5d&query=${lat},${long}&units=m`;
 
     request({ url, json: true }, (err, { body }) => {
         if (err) {
-            callback('Unable to connect to weather service')
+            callback('Unable to connect to weather service');
         } else if (body.error) {
-            callback('Unable to find location')
+            callback('Unable to find location');
         } else {
             const {
                 weather_descriptions: descriptions,
                 temperature,
                 feelslike,
-            } = body.current
+                humidity,
+            } = body.current;
             callback(
                 undefined,
-                `${descriptions[0]}. It is currently ${temperature} degres outside. It feels like ${feelslike} degrees`
-            )
+                `${descriptions[0]}. It is currently ${temperature} degres outside. It feels like ${feelslike} degrees. There is a humidity of ${humidity}%`
+            );
         }
-    })
-}
+    });
+};
 
-module.exports = forecast
+module.exports = forecast;
